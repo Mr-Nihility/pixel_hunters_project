08a67f00016cefbd12945123aa745d78e84c722d
Volodymyr Romanenko <romanonebe@gmail.com>
2022-05-11 10:34:54 +0300

    js fix

--- src/js/mobile_menu.js
+++ src/js/mobile_menu.js
@@ -1,16 +1,22 @@
 (() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
-  const body = document.querySelector("body");
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const innerBtnRef = document.querySelector('[data-inner-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+  const body = document.querySelector('body');
+  const modal = document.querySelector('[data-modal]');
 
-  menuBtnRef.addEventListener("click", () => {
-    const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+  menuBtnRef.addEventListener('click', () => {
+    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
 
-    mobileMenuRef.classList.toggle("is-open");
-    body.classList.toggle("no-scroll");
+    mobileMenuRef.classList.toggle('is-open');
+    body.classList.toggle('no-scroll');
+  });
+  innerBtnRef.addEventListener('click', () => {
+    mobileMenuRef.classList.toggle('is-open');
+    modal.classList.toggle('is-hidden');
+    menuBtnRef.classList.toggle('is-open');
   });
 })();
